4cfa96bd6dcec65acb678c91f4b738c7f08f46c7
Devjadoun <devjadoun04@gmail.com>
2026-09-21 14:48:48 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 112233
+let accountEmail = "devjadoun04@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+// accountId = 2 //not allowed
+
+accountEmail = "hchc@gmail.com"
+accountPassword = "121212"
+accountCity = "bangalore"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
