2e17b451051025fc463733c8f09269634dabdf72
Devjadoun <devjadoun04@gmail.com>
2026-09-21 14:49:10 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict" //treat aa JS code as newer version
+
+//alert(3+2)  // we are using nodejs, not browser
+
+
+let name = "Dev"
+let age  = 20
+let isLoggedIn = false
+let state = null
+
+
+// number => 2 to power 53
+// big int
+// string => ""
+// boolean => true/false
+// null => standAlone Value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+
+console.log(typeof "Dev");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
