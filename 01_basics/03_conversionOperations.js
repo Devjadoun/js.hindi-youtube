979abaa614b1e3708d1f58b7b8277c83840626a6
Devjadoun <devjadoun04@gmail.com>
2026-09-24 04:55:26 +0000

    string conversion confusion

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -27,3 +27,31 @@ console.log(StringNumber);
 console.log(typeof StringNumber);
 
 
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+
+
+let str1 = "hello"
+let str2 = " dev"
+let str3 = str1 + str2
+// console.log(str3)
+// console.log("1" +2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// // comsole.log((3 + 4) * 5 % 3)
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
+
+
